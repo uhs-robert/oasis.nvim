84e26aa1bc4c67539966e2f00da52c6ef95e72d9
Robert Hill <robert.hill@uphillsolutions.tech>
2025-12-08 23:36:09 -0500

    fix: temporarily disable Lua treesitter PascalCase and ALL_CAPS highlights
    Commented out treesitter highlight rules for PascalCase variables as
    types
    and ALL_CAPS variables as constants in
    'after/queries/lua/highlights.scm'.
    This change added extra noide, might be better to leave as the default.
    Will experiment over next few days.

--- after/queries/lua/highlights.scm
+++ after/queries/lua/highlights.scm
@@ -1,11 +1,11 @@
 ;; extends
 
-; PascalCase variables as Type
-((identifier) @type
-  (#match? @type "^[A-Z][A-Za-z0-9_]*$")
-  (#set! priority 115))
-
-; ALL_CAPS variables are Constant
-((identifier) @constant
-  (#match? @constant "^[A-Z][A-Z0-9_]*$")
-  (#set! priority 120))
+; ; PascalCase variables as Type
+; ((identifier) @type
+;   (#match? @type "^[A-Z][A-Za-z0-9_]*$")
+;   (#set! priority 115))
+;
+; ; ALL_CAPS variables are Constant
+; ((identifier) @constant
+;   (#match? @constant "^[A-Z][A-Z0-9_]*$")
+;   (#set! priority 120))
