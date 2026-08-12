394c2fc23e66c1e5984687bddfc86bc0f172d80f
Robert Hill <robert.hill@uphillsolutions.tech>
2026-08-11 22:40:45 -0400

    fix(themes): lighten func/builtin_func shade one step

--- lua/oasis/color_palettes/oasis_desert.lua
+++ lua/oasis/color_palettes/oasis_desert.lua
@@ -67,7 +67,7 @@ local dark = {
     constant = p.sunset[500], -- (constant: number, float, boolean, or const not string/character)
 
     -- Warm: (Control / Flow)
-    func = p.copper[300],
+    func = p.copper[200],
     builtin_func = p.copper[400], -- (eg. parseInt, Array, Object etc)
     statement = primary[500], -- (general statement (i.e. var, const))
     conditional = primary[800], -- (Conditionals, Loops)
--- lua/oasis/color_palettes/oasis_night.lua
+++ lua/oasis/color_palettes/oasis_night.lua
@@ -57,8 +57,8 @@ local dark = {
     constant = p.sunset[600], -- (constant: number, float, boolean, or const not string/character)
 
     -- Warm: (Control / Flow)
-    func = p.copper[300],
-    builtin_func = p.copper[500], -- (eg. parseInt, Array, Object etc)
+    func = p.copper[200],
+    builtin_func = p.copper[400], -- (eg. parseInt, Array, Object etc)
     statement = base.palette.primary[500], -- (general statement (i.e. var, const))
     conditional = base.palette.primary[800], -- (Conditionals, Loops)
     exception = p.red[500], -- (try/catch, return)
